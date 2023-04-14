b54a1ba2aa71d2ebf68822a6c84e25314d1f035e
LiDLine <artyom.zhevnerenko@yandex.ru>
2023-04-14 19:58:43 +0300

    some edits

--- main.py
+++ main.py
@@ -17,18 +17,19 @@ def solve(x, y, z, alpha, beta, gamma):
 
     move_upper_CS = array((x, y, z, alpha, beta, gamma))    # Смещение центра верхней плиты
 
-    """"3. Координаты плит"""
+    """"2. Координаты плит"""
 
     matrix_move_lower_plate, matrix_move_upper_plate, lower_points, upper_points = plate_coordinates(move_upper_CS, offset_lower, rotate_local_cs, local_lower_CS, offset_upper, local_upper_CS)
   
-    """4. Точки пересечения"""
+    """3. Точки пересечения"""
     arm_angle, angle_lower_joint, angle_upper_joint = leg_ball_corners(matrix_move_lower_plate, upper_points, matrix_move_upper_plate, border_of_arm_angle, R, r)
 
-    """5. Поиск углов шаровых наконечников пружин"""
+    """4. Поиск углов шаровых наконечников пружин"""
 
     angle_lower_joint, angle_upper_joint = spring_ball_corners(upper_points, lower_points, matrix_move_lower_plate, matrix_move_upper_plate, angle_lower_joint, angle_upper_joint, moving_gas_spring)
 
-    """6. Проверка углов шаровых опор"""
+    """5. Проверка углов шаровых опор"""
+    
     # Если у одной шаровой угол > max_angle_spherical_joint - отметаем решение
     for i in range(9):
         if abs(angle_lower_joint[i]*180/pi) > max_angle_spherical_joint or abs(angle_upper_joint[i]*180/pi) > max_angle_spherical_joint:
